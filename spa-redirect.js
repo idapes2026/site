6fadfa345ce5d847f74414c2caeb06a622f34523
idapes2026 <contato@idapes.com.br>
2026-03-03 18:16:53 -0300

    Update spa-redirect.js

--- spa-redirect.js
+++ spa-redirect.js
@@ -1,17 +1,36 @@
-// GitHub Pages SPA Redirect
-// Preserva a rota acessada e redireciona corretamente para o React Router
-
+// GitHub Pages SPA Redirect (works for both root site and project subpath)
+// - If hosted at https://user.github.io/ -> basePath = ""
+// - If hosted at https://user.github.io/repo -> basePath = "/repo"
 (function () {
-  var path = window.location.pathname;
+  try {
+    var pathname = window.location.pathname || "/";
 
-  // Remove a parte do repositório do caminho se existir
-  var repo = "/site";
+    // Detect basePath:
+    // If your Pages is a project site, the first segment is usually the repo name.
+    // If it's a user/org site, it's served from root.
+    var segments = pathname.split("/").filter(Boolean);
 
-  if (path.startsWith(repo)) {
-    path = path.substring(repo.length);
+    // If the first segment looks like a repo name AND the request is not just "/",
+    // we can treat it as basePath. Otherwise, basePath is root.
+    // HOWEVER: on user-site, the first segment is a real route, so basePath must be "".
+    // To support both safely, we ONLY set basePath if you explicitly are using a known subpath.
+    // Here we auto-detect by checking if a 404.html is being served from a subpath:
+    // If current pathname starts with "/site/" OR equals "/site", basePath="/site".
+    // Otherwise basePath="" (root).
+    var basePath = "";
+    if (pathname === "/site" || pathname.startsWith("/site/")) {
+      basePath = "/site";
+      pathname = pathname.substring(basePath.length) || "/";
+    }
+
+    // Normalize to always start with "/"
+    if (!pathname.startsWith("/")) pathname = "/" + pathname;
+
+    var redirectUrl =
+      (basePath ? basePath : "") + "/?redirect=" + encodeURIComponent(pathname);
+
+    window.location.replace(redirectUrl);
+  } catch (e) {
+    // fail-safe: do nothing
   }
-
-  var redirect = repo + "/?redirect=" + encodeURIComponent(path);
-
-  window.location.replace(redirect);
 })();
